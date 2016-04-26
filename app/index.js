08f19204c93f4f7c49c69f582831a3def6a5c6bf
sztukamichal <sztuuka.michal@gmail.com>
2016-04-26 23:40:33 +0200

    Demo sidenav added.

--- app/index.js
+++ app/index.js
@@ -2,13 +2,14 @@
 
 var angular = require('angular');
 require('angular-ui-router');
-require('angular-material');
-require('angular-animate');
 require('angular-aria');
+require('angular-animate');
+require('angular-material');
 require('./home/index');
-
+require('angular-material');
 
 angular.module('betOnMe', [
   require('angular-ui-router'),
+  'ngMaterial',
   'home'
 ]);
