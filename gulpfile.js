dec701c558ef3e56972d99ef030d82a3ca4e52b0
Michał <sztuuka.michal@gmail.com>
2016-04-26 16:09:45 +0200

    Defined gulp tasks - browserify, html, war and build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,31 @@
 'use strict';
 
 var gulp = require('gulp');
+var war = require('gulp-war');
+var zip = require('gulp-zip');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('browserify', function() {
+  return browserify('app/index.js').bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist/build/app'))
+    .pipe(gulp.dest('app/'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('app/**/*.html')
+    .pipe(gulp.dest('dist/build/app'));
+});
+
+gulp.task('war', ['browserify', 'html'], function() {
+  return gulp.src('dist/build/**')
+    .pipe(war({
+      welcome: 'index.html',
+      displayName: 'Bet on me'
+    }))
+    .pipe(zip('betonme.war'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['war']);
