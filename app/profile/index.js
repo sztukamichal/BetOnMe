cccc1365bb36a5a108c8f9f79e4ea1c6768f7e9b
Michal Sztuka <michal.sztuka@stazysta.comarch.pl>
2016-04-27 17:29:32 +0200

    Added images to git repository.

--- app/profile/index.js
+++ app/profile/index.js
@@ -5,6 +5,10 @@ var profile = angular.module('profile', [])
   .controller('ProfileController', require('./profile-controller.js'))
   .config(function($mdThemingProvider) {
     $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+    $mdThemingProvider.theme('light-green').backgroundPalette('light-green').dark();
+    $mdThemingProvider.theme('lime').backgroundPalette('lime').dark();
+    $mdThemingProvider.theme('amber').backgroundPalette('amber').dark();
+    $mdThemingProvider.theme('yellow').backgroundPalette('yellow').dark();
     $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
     $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
     $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
