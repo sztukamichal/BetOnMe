8ae47c997aaf17136c780374ff9ab399245f2f6c
sztukamichal <sztuuka.michal@gmail.com>
2016-06-05 14:04:29 +0200

    Small fixes

--- server/controllers/api/soccerSeasons.js
+++ server/controllers/api/soccerSeasons.js
@@ -19,10 +19,28 @@ router.get('/', function (req, res) {
   }
 });
 
+router.get('/fixtures', function (req, res) {
+  if(req.auth && req.auth.username) {
+    if(req.query.timeFrame !== undefined) {
+      SoccerSeason.getFixturesByDate(undefined, req.query.timeFrame, function (err, result) {
+        if (err) {
+          res.sendStatus(500);
+          return;
+        }
+        res.json(result);
+      });
+    } else {
+      res.sendStatus(400);
+    }
+  } else {
+    return res.sendStatus(401);
+  }
+});
+
 router.get('/fixtures/:seasonId', function (req, res) {
   if(req.auth && req.auth.username) {
     if(req.query.timeFrame !== undefined) {
-      SoccerSeason.getFixturesByDate(req.query.timeFrame, function (err, result) {
+      SoccerSeason.getFixturesByDate(req.params.seasonId, req.query.timeFrame, function (err, result) {
         if (err) {
           res.sendStatus(500);
           return;
--- server/models/soccerseason.js
+++ server/models/soccerseason.js
@@ -163,37 +163,64 @@ SoccerSeasonSchema.statics.findAllSeasonsWithoutFixtures = function (callback) {
   this.find({}, {"fixtures": 0}, callback);
 };
 
-SoccerSeasonSchema.statics.getFixturesByDate = function (timeFrame, callback) {
-  var from = new Date(),
-    to = new Date(),
-    prefix = timeFrame.slice(0,1),
-    timeFrame = timeFrame.slice(1,timeFrame.length),
-    days = parseInt(timeFrame);
-  from.setHours(from.getHours() + 2);
-  to.setHours(to.getHours() + 2);
-  days = prefix === 'n' ? days : -days;
-  to.setDate(to.getDate() + days);
-  if(from > to) {
-    var tmp = from;
-    from = to;
-    to = tmp;
+SoccerSeasonSchema.statics.getFixturesByDate = function (id, timeFrame, callback) {
+  function getDateRange(timeFrame) {
+    var from = new Date(),
+      to = new Date(),
+      prefix = timeFrame.slice(0, 1),
+      timeFrame = timeFrame.slice(1, timeFrame.length),
+      days = parseInt(timeFrame);
+    from.setHours(from.getHours() + 2);
+    to.setHours(to.getHours() + 2);
+    days = prefix === 'n' ? days : -days;
+    to.setDate(to.getDate() + days);
+    if (from > to) {
+      var tmp = from;
+      from = to;
+      to = tmp;
+    }
+    from = from.toISOString();
+    to = to.toISOString();
+    return {from: from, to: to};
   }
-  from = from.toISOString();
-  to = to.toISOString();
-    this.aggregate([
-      {$unwind: "$fixtures.fixtures"},
-      {
-        $match: {
-          'fixtures.fixtures.date': {$gte: new Date(from), $lte: new Date(to)}
-        }
-      },
-      {
-        $group: {
-          _id: {id: "$id", caption: "$caption"},
-          fixtures: {$push: "$fixtures.fixtures"}
-        }
+
+  var __ret = getDateRange(timeFrame);
+  var from = __ret.from;
+  var to = __ret.to;
+  var pipelinesWithId = [
+    {$match : {id: id}},
+    {$unwind: "$fixtures.fixtures"},
+    {
+      $match: {
+        'fixtures.fixtures.date': {$gte: new Date(from), $lte: new Date(to)}
       }
-    ]).exec(callback);
+    },
+    {
+      $group: {
+        _id: {id: "$id", caption: "$caption"},
+        fixtures: {$push: "$fixtures.fixtures"}
+      }
+    }
+  ];
+  var pipelinesWithoutId = [
+    {$unwind: "$fixtures.fixtures"},
+    {
+      $match: {
+        'fixtures.fixtures.date': {$gte: new Date(from), $lte: new Date(to)}
+      }
+    },
+    {
+      $group: {
+        _id: {id: "$id", caption: "$caption"},
+        fixtures: {$push: "$fixtures.fixtures"}
+      }
+    }
+  ];
+  if(id !== undefined) {
+    this.aggregate(pipelinesWithId).exec(callback);
+  } else {
+    this.aggregate(pipelinesWithoutId).exec(callback);
+  }
 };
 
 
